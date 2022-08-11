84b340865d05b1c3188804b77ea856b67be8b283
Roman Savran <roblumination@gmail.com>
2022-08-11 15:51:30 +0300

    add slider murkup

---
+++ src/scripts/singleSlider.js
@@ -0,0 +1,7 @@
+const slider = {
+  arrow: {
+    left: document.getElementById("slid-arrow-1L"),
+    right: document.getElementById("slid-arrow-1R"),
+  },
+  extender: document.getElementById("ext-1"),
+};
