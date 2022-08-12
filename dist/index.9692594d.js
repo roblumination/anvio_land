5726426541af56c1096815dacc1c6143c917380f
Roman Savran <roblumination@gmail.com>
2022-08-12 16:54:27 +0300

    add dist

---
+++ dist/index.9692594d.js
@@ -0,0 +1,59 @@
+const buttons = {
+    location: document.getElementById("header-btn-location"),
+    phone: document.getElementById("header-btn-phone"),
+    menu: document.getElementById("header-btn-menu")
+};
+const menuIco = {
+    closed: document.getElementById("menu-ico_closed"),
+    opened: document.getElementById("menu-ico_opened")
+};
+buttons.location.addEventListener("click", ()=>{
+    state.modalSmallLocationIsActive = !state.modalSmallLocationIsActive;
+    state.modalSmallMenuIsActive = false;
+    state.modalIsActive = state.modalSmallLocationIsActive ? true : false;
+    render();
+});
+buttons.menu.addEventListener("click", ()=>{
+    state.modalSmallMenuIsActive = !state.modalSmallMenuIsActive;
+    state.modalIsActive = state.modalSmallMenuIsActive ? true : false;
+    state.modalSmallLocationIsActive = false;
+    render();
+});
+const modals = {
+    background: document.querySelector(".modal__background"),
+    menu: document.getElementById("modal-menu"),
+    location: document.getElementById("modal-location")
+};
+modals.background.addEventListener("click", (e)=>{
+    if (e.target != modals.background) return;
+    state.modalIsActive = false;
+    state.modalSmallLocationIsActive = false;
+    state.modalSmallMenuIsActive = false;
+    render();
+});
+let state = {
+    // need to close modal without background
+    modalIsActive: false,
+    modalSmallLocationIsActive: false,
+    modalSmallMenuIsActive: false
+};
+function render() {
+    // apply styles according to state{}
+    //background
+    modals.background.style.opacity = state.modalIsActive ? "1" : "0.0";
+    modals.background.style.pointerEvents = state.modalIsActive ? "all" : "none";
+    //menu
+    modals.menu.style.transform = state.modalSmallMenuIsActive ? "translateX(0px)" : "translateX(300px)";
+    modals.menu.style.opacity = state.modalSmallMenuIsActive ? "1" : "0.0";
+    modals.menu.style.pointerEvents = state.modalSmallMenuIsActive ? "all" : "none";
+    buttons.menu.style.backgroundColor = state.modalSmallMenuIsActive ? "#fff" : "#fff0";
+    menuIco.closed.style.display = state.modalSmallMenuIsActive ? "none" : "block";
+    menuIco.opened.style.display = !state.modalSmallMenuIsActive ? "none" : "block";
+    //location
+    modals.location.style.transform = state.modalSmallLocationIsActive ? "translateX(0px)" : "translateX(300px)";
+    modals.location.style.opacity = state.modalSmallLocationIsActive ? "1" : "0.0";
+    modals.location.style.pointerEvents = state.modalSmallLocationIsActive ? "all" : "none";
+    buttons.location.style.backgroundColor = state.modalSmallLocationIsActive ? "#fff" : "#fff0";
+} // setTimeout(() => render(), 1000);
+
+//# sourceMappingURL=index.9692594d.js.map
---
+++ dist/index.de937c7a.js
@@ -0,0 +1,32 @@
+const slider = {
+    arrow: {
+        left: document.getElementById("slid-arrow-1L"),
+        right: document.getElementById("slid-arrow-1R")
+    },
+    extender: document.getElementById("ext-1"),
+    indicators: document.getElementsByClassName("indicator-dot"),
+    position: 0,
+    maxPosition: 2
+};
+slider.arrow.left.addEventListener("click", ()=>{
+    if (slider.position == 0) slider.position = slider.maxPosition;
+    else slider.position--;
+    slider.extender.style.transform = `translateX(-${100 / (slider.maxPosition + 1) * slider.position}%)`;
+    offAllIndicators();
+    slider.indicators[slider.position].style.opacity = "1";
+});
+slider.arrow.right.addEventListener("click", ()=>{
+    if (slider.position == slider.maxPosition) slider.position = 0;
+    else slider.position++;
+    slider.extender.style.transform = `translateX(-${100 / (slider.maxPosition + 1) * slider.position}%)`;
+    offAllIndicators();
+    slider.indicators[slider.position].style.opacity = "1";
+});
+function offAllIndicators() {
+    // console.log(slider.indicators[slider.position]);
+    [].forEach.call(slider.indicators, (ind)=>{
+        ind.style.opacity = "0.5";
+    });
+}
+
+//# sourceMappingURL=index.de937c7a.js.map
